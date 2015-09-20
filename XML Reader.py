7924058cdf43f7d50a327613bb904224b526882a
joe <ioanna.kolovou@gmail.com>
2015-09-20 12:56:25 +0100

    XML Reader

--- XML Reader.py
+++ XML Reader.py
@@ -1,4 +1,3 @@
-__author__ = 'joe'
 import xml.etree.cElementTree as et
 
 tree = et.parse('/Users/joe/Desktop/TfL_Santander Cycles availability.xml')
@@ -11,37 +10,29 @@ from PyQt4.QtCore import QVariant
 Points = QgsVectorLayer ( 'Point', 'Points', "memory")
 Points.startEditing()
 
-Points.addAttributes( [ QgsField("name", QVariant.String), QgsField("id",  QVariant.Int), QgsField("long",  QVariant.Double), QgsField("lat",QVariant.Double)])
-
-
-"""for station in root.findall('station'):
-	id= station.find('id').text
-	name = station.find('name').text
-	long = station.find ('long').text
-	lat = station.find ('lat').text
-	station=QgsFeature()
-	station.setGeometry( QgsGeometry.fromPoint(QgsPoint(float(lat),float(long))))
-	station.setAttribute ({0: QVariant(name), 1:QVariant(int(id)) , 2:QVariant(float(long)) , 3:QVariant(float(lat)) })
-	pr.addFeatures ([station])
+pr=Points.dataProvider()
+pr.addAttributes([QgsField("name", QVariant.String), QgsField("id",  QVariant.Int), QgsField("long",  QVariant.Double), QgsField("lat",QVariant.Double)])
 
 Points.commitChanges()
-QgsMapLayerRegistry.instance().addMapLayer(Points)"""
 
+#You have to commit changes to the vector layer for using pedingFileds() method. Otherwise it will return an empty list
+fields=Points.pendingFields()
+
+Points.startEditing()
 
 for station in root.findall('station'):
-	id= station.find('id').text
-	name = station.find('name').text
-	lat = station.find ('lat').text
-	long = station.find ('long').text
-	station=QgsFeature()
-	station.setGeometry( QgsGeometry.fromPoint(QgsPoint(float(long),float(lat))))
-	fields=pr.fields()
-	station.setFields( fields, True)
-	station['id']= int(id)
-	station['name']=str(name)
-	station['lat']=float(lat)
-	station['long']=float(long)
-	Points.addFeatures ([station])
+    id= station.find('id').text
+    name = station.find('name').text
+    lat = station.find ('lat').text
+    long = station.find ('long').text
+    station=QgsFeature()
+    station.setGeometry( QgsGeometry.fromPoint(QgsPoint(float(long),float(lat))))
+    station.setFields( fields, True)
+    station['id']= int(id)
+    station['name']=str(name)
+    station['lat']=float(lat)
+    station['long']=float(long)
+    Points.addFeatures ([station])
 
 Points.commitChanges()
 QgsMapLayerRegistry.instance().addMapLayer(Points)
