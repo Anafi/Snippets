b05e892a0772507c809fb1a36380b47cc35a9630
Ioanna <ioanna.kolovou@gmail.com>
2016-06-07 17:57:12 +0100

    initial commit

---
+++ JSON_to_GeoJSON.py
@@ -0,0 +1,15 @@
+
+# copied from
+def convert_json(items):
+    import json
+    return json.dumps(dict(type="FeatureCollection", features=[
+        {"type": "Feature",
+         "geometry": {"type": "Point",
+                      "coordinates": [feature['lon'],
+                                      feature['lat']]},
+         "properties": {key: value
+                        for key, value in feature.items()
+                        if key not in ('lat', 'lon')}
+         }
+        for feature in json.loads(items)
+        ]))
