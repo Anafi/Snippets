62a843ac31487ac8504232788789771a84ac623d
Ioanna <ioanna.kolovou@gmail.com>
2016-06-07 15:53:13 +0100

    bus routes added

--- bus_stops_to_routes.py
+++ bus_stops_to_routes.py
@@ -8,6 +8,9 @@
 # load dependencies
 import math
 from qgis.core import *
+from itertools import groupby
+from operator import itemgetter
+import networkx as nx
 
 # define line layer of your network (n) and point layer representing the bus stops (p)
 n = iface.mapCanvas().currentLayer()
@@ -70,7 +73,7 @@ for k, v in p_to_lines.items():
         closest_line = v[0]
     else:
         closest_line = sorted_lines[0]
-    if len(sorted_lines)>1:
+    if len(sorted_lines) > 1:
         if (line_class[sorted_lines[1]] == u'A Road' or line_class[sorted_lines[1]]== u'B Road') and not (line_class[sorted_lines[0]] == u'A Road' or line_class[sorted_lines[0]]== u'B Road') :
             closest_line = sorted_lines[1]
     p_to_lines[k] = [closest_line]
@@ -81,3 +84,80 @@ for k, v in p_to_lines.items():
     lines.append(v[0])
 
 n.select(lines)
+
+################################################################################################
+# group by route and run
+bus_routes=[]
+for point in p.getFeatures():
+    route = point.attributes()[1]
+    seq = int(point.attributes()[3])
+    run = int(point.attributes()[2])
+    bus_routes.append([point.id(), route, run, seq])
+
+Routes = {}
+for route, iter in groupby(bus_routes, itemgetter(1)):
+    list = []
+    for i in iter:
+        list.append(i)
+    for j, v in groupby(list,itemgetter(2)):
+        list_2 = []
+        for k in v:
+            list_2.append(k)
+        Routes[route, j] = list_2
+
+for i, v in Routes.items():
+    v.sort(key=itemgetter(3))
+
+# find pairs of sorted grouped line segments
+pairs = []
+for k, v in Routes.items():
+    list = []
+    for i in v:
+        list.append(i[0])
+    for v, w in zip(list[:-1], list[1:]):
+        pairs.append([v, w])
+
+seg_pairs = []
+for i in pairs:
+    seg_pair = p_to_lines[i[0]] + p_to_lines[i[1]]
+    seg_pairs.append(seg_pair)
+
+D = {}
+for i in n.getFeatures():
+    D[i.id()] = [(i.geometry().asPolyline()[0][0], i.geometry().asPolyline()[0][1]), (i.geometry().asPolyline()[-1][0], i.geometry().asPolyline()[-1][1]) ]
+
+point_pairs = []
+for i in seg_pairs:
+    point_pairs.append([D[i[0]][0],D[i[1]][0]])
+
+
+G = nx.Graph()
+for f in n.getFeatures():
+    f_geom = f.geometry()
+    id = f.id()
+    p0 = (f_geom.asPolyline()[0][0], f_geom.asPolyline()[0][1])
+    p1 = (f_geom.asPolyline()[-1][0], f_geom.asPolyline()[-1][1])
+    G.add_edge(p0, p1, {'fid': id})
+
+# TO DO: add cost to the network based on class type
+paths = []
+for i in point_pairs:
+    paths.append(nx.shortest_path(G, source=i[0], target=i[1]))
+
+endpoints_to_keep = []
+for path in paths:
+    for i in path:
+        if i not in endpoints_to_keep:
+            endpoints_to_keep.append(i)
+
+bus_route_paths = []
+for k, v in D.items():
+    if v[0] in endpoints_to_keep and v[1] in endpoints_to_keep:
+        if k not in bus_route_paths:
+            bus_route_paths.append(k)
+
+for k, v in p_to_lines.items():
+    if v[0] not in bus_route_paths:
+        bus_route_paths.append(v[0])
+
+n.select(bus_route_paths)
