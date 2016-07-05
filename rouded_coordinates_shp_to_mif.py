8edd39d3b8feafd3bc37bc963a62b7930871bedc
Ioanna <ioanna.kolovou@gmail.com>
2016-07-05 15:11:22 +0100

    three methods

--- rouded_coordinates_shp_to_mif.py
+++ rouded_coordinates_shp_to_mif.py
@@ -15,13 +15,6 @@ fieldIdx = n.dataProvider().fields().indexFromName("wkt")
 
 updateMap_geom = {}
 
-# find the maximum and the minimum number of digits for x-s and y-s
-max_digit_x = 0
-min_digit_x = 100000000
-max_digit_y = 0
-min_digit_y = 100000000
-
-
 def digit_sum(n):
     n = abs(n)
     total = 0
@@ -30,46 +23,42 @@ def digit_sum(n):
         n = n // 10
     return total
 
+def get_two_decimals(number):
+    d = number - int(number)
+    two_decimals = float(str(d)[0:4])
+    new_number = int(number) + two_decimals
+    return new_number
 
-# digit_start_x = digit_sum(f.geometry().asPolyline()[0][0])
-# digit_end_x = digit_sum(f.geometry().asPolyline()[1][0])
-# if digit_start_x > max_digit_x:
-#    max_digit_x = digit_start_x
-# if digit_end_x > max_digit_x:
-#    max_digit_x = digit_end_x
-# if digit_start_x < min_digit_x:
-#    min_digit_x = digit_start_x
-# if digit_end_x < min_digit_x:
-#    min_digit_x = digit_end_x
-# digit_start_y = digit_sum(f.geometry().asPolyline()[0][1])
-# digit_end_y = digit_sum(f.geometry().asPolyline()[1][1])
-# if digit_start_y > max_digit_y:
-#    max_digit_y = digit_start_y
-# if digit_end_y > max_digit_y:
-#    max_digit_y = digit_end_y
-# if digit_start_y < min_digit_y:
-#    min_digit_y = digit_start_y
-# if digit_end_y < min_digit_y:
-#    min_digit_y = digit_end_y
-
+# method 1 by rounding up (you need to v.clean-snap afterwards)
 for f in n.getFeatures():
-    fid=f.id()
+    fid = f.id()
     start_point_x = round(f.geometry().asPolyline()[0][0], 0)
-    # if len(str(math.modf(start_point_x)[0])) == 4:
-    #    start_point_x = str(start_point_x)+'0'
     start_point_y = round(f.geometry().asPolyline()[0][1], 0)
-    # if len(str(math.modf(start_point_y)[0])) == 4:
-    #    start_point_y = str(start_point_y) + '0'
     end_point_x = round(f.geometry().asPolyline()[1][0], 0)
-    # if len(str(math.modf(end_point_x)[0])) == 4:
-    #    end_point_x = str(end_point_x) + '0'
     end_point_y = round(f.geometry().asPolyline()[1][1], 0)
-    # if len(str(math.modf(end_point_y)[0])) == 4:
-    #    start_end_y = str(end_point_y) + '0'
     wkt_geom = 'LineString ('+str(start_point_x)+' '+str(start_point_y)+', '+str(end_point_x)+' '+str(end_point_y)+')'
     updateMap_geom[fid] = {fieldIdx: wkt_geom}
 
+# method 2 by keeping the two decimals (still number is float)
+for f in n.getFeatures():
+    fid = f.id()
+    start_point_x = get_two_decimals(f.geometry().asPolyline()[0][0])
+    start_point_y = get_two_decimals(f.geometry().asPolyline()[0][1])
+    end_point_x = get_two_decimals(f.geometry().asPolyline()[1][0])
+    end_point_y = get_two_decimals(f.geometry().asPolyline()[1][1])
+    wkt_geom = 'LineString ('+str(start_point_x)+' '+str(start_point_y)+', '+str(end_point_x)+' '+str(end_point_y)+')'
+    updateMap_geom[fid] = {fieldIdx: wkt_geom}
 
-n.dataProvider().changeAttributeValues( updateMap_geom )
+# method 3 by converting to integers
+for f in n.getFeatures():
+    fid = f.id()
+    start_point_x = int(f.geometry().asPolyline()[0][0])
+    start_point_y = int(f.geometry().asPolyline()[0][1])
+    end_point_x = int(f.geometry().asPolyline()[1][0])
+    end_point_y = int(f.geometry().asPolyline()[1][1])
+    wkt_geom = 'LineString ('+str(start_point_x)+' '+str(start_point_y)+', '+str(end_point_x)+' '+str(end_point_y)+')'
+    updateMap_geom[fid] = {fieldIdx: wkt_geom}
+
+n.dataProvider().changeAttributeValues(updateMap_geom)
 
 
