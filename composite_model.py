fb017be6d66313b9e7a1614453c01d098de09212
Ioanna <ioanna.kolovou@gmail.com>
2016-07-28 18:03:19 +0100

    added function to extend lines

--- composite_model.py
+++ composite_model.py
@@ -27,7 +27,7 @@ os_road_sel = QgsVectorLayer(os_road_sel_shp_path, "os_road_sel", "ogr")
 
 # select junctions and roadEnd from os_road_nodes
 
-expr_os_node = QgsExpression("formOfNode= 'junction' OR formOfNode='roadEnd'")
+expr_os_node = QgsExpression("formOfNode= 'junction' OR formOfNode='roadEnd' OR formOfNode='roundabout'")
 
 os_node_shp_path = 'C:\Users\I.Kolovou\Desktop\itn\osroadnode_sample.shp'
 os_node = QgsVectorLayer(os_node_shp_path, "os_node_sample", "ogr")
@@ -80,49 +80,147 @@ update_feat_id_col(os_road_sel, 'feat_id', 0)
 QgsMapLayerRegistry.instance().addMapLayer(os_road_sel)
 os_road_graph = read_shp_to_graph(os_road_sel_shp_path)
 
-'''filter connectivity of nodes that are within a buffer from the meridian'''
+# TODO: filter connectivity of nodes that are within a buffer from the meridian
+# TODO: use identifier (os_node) and startNode - endNode instead (os_road) OR build graph based on identifier
 nodes_con_1 = [node for node in os_road_graph.nodes() if len(os_road_graph.neighbors(node)) == 1]
 
 junctions = {}
+junction_to_join_coords = []
 roadEnds = {}
 for i in nodes_con_1:
     attr = os_node_dict.get((int(i[0]), int(i[1])))
     if attr is None:
         pass
     else:
-        formOfNode= attr[1]
-        if formOfNode == 'junction':
+        formOfNode = attr[1]
+        if formOfNode == 'junction' or formOfNode == 'roundabout':
             junctions[i] = attr[1]
+            junction_to_join_coords.append((int(i[0]), int(i[1])))
         elif formOfNode == 'roadEnd':
             roadEnds[i] = attr[1]
 
-edges_to_join = []
-edges_to_check = []
-for e in os_road_graph.edges(data=True):
-    if e[0] in junctions.keys() or e[1] in junctions.keys():
-        edges_to_join.append(e[2]['feat_id'])
-    elif e[0] in roadEnds.keys() or e[1] in roadEnds.keys():
-        edges_to_check.append(e[2]['feat_id'])
 
-os_road_sel = iface.mapCanvas().currentLayer()
-os_road_sel.select(edges_to_join)
+edge_dict = {i.id(): i.geometry().asPolyline() for i in os_road_sel.getFeatures()}
+edge_feat_dict = {i.id(): i for i in os_road_sel.getFeatures()}
+meridian_geom_dict = {i.id(): i.geometryAndOwnership() for i in meridian.getFeatures()}
 
-'''use identifier (os_node) and startNode - endNode instead (os_road) OR build graph based on identifier'''
-
-# find which features from os_road intersect the buffer of meridian
-provider = os_road_sel.dataProvider()
+provider = meridian.dataProvider()
 spIndex = QgsSpatialIndex()  # create spatial index object
 feat = QgsFeature()
 fit = provider.getFeatures()  # gets all features in layer
 # insert features to index
 while fit.nextFeature(feat):
     spIndex.insertFeature(feat)
+# find lines intersecting other lines
+closest_lines = {(int(i[0]),int(i[1])): spIndex.nearestNeighbor(QgsPoint(i[0], i[1]), 1) for i in junction_to_join_coords}
 
-os_road_meridian = {i.id(): spIndex.intersects(QgsRectangle()) for i in meridian_buffer.getFeatures() if not i.geometry().isMultipart()}
+def PointEquality(vertex1,vertex2):
+    return ((abs(vertex1[0] - vertex2[0]) < 0.000001) and
+            (abs(vertex1[1] - vertex2[1]) < 0.000001))
 
-#(nearest point of one geom to another)
-# QgsGeometry  nearestPoint (const QgsGeometry &other) const
-# QgsGeometry  shortestLine (const QgsGeometry &other) const
+edges_to_join = {}
+for e in os_road_graph.edges(data=True):
+    ind = len(edge_dict[e[2]['feat_id']])-1
+    # TODO: if there is another vertex closer to the new point use this vertex and delete the remaining vertices
+    if e[0] in junctions.keys():
+        closest_line = closest_lines[(int(e[0][0]), int(e[0][1]))]
+        if (int(edge_dict[e[2]['feat_id']][0][0]), int(edge_dict[e[2]['feat_id']][0][1])) == (int(e[0][0]),int(e[0][1])):
+            ind = 0
+        # TODO: check which of the closest lines to use
+        new_point = meridian_geom_dict[closest_line[0]].nearestPoint(QgsGeometry().fromPoint(QgsPoint(e[0][0], e[0][1])))
+        # eliminate those that the closest_point is the vertex of the segment to be joined to the meridian
+        if not PointEquality(new_point.asPoint(), e[0]):
+            edges_to_join[e[0]] = {'line':e[2]['feat_id'], 'index':ind,'closest_neigh':closest_line, 'closest_point':new_point.asPoint()}
+    elif e[1] in junctions.keys():
+        closest_line = closest_lines[(int(e[1][0]), int(e[1][1]))]
+        if (int(edge_dict[e[2]['feat_id']][0][0]), int(edge_dict[e[2]['feat_id']][0][1])) == (int(e[1][0]), int(e[1][1])):
+            ind = 0
+        # TODO: check which of the closest lines to use
+        new_point = meridian_geom_dict[closest_line[0]].nearestPoint(QgsGeometry().fromPoint(QgsPoint(e[1][0], e[1][1])))
+        # eliminate those that the closest_point is the vertex of the segment to be joined to the meridian
+        if not PointEquality(new_point.asPoint(), e[1]):
+            edges_to_join[e[1]] = {'line':e[2]['feat_id'], 'index': ind, 'closest_neigh': closest_line, 'closest_point': new_point.asPoint()}
 
+feat_to_del = []
+new_feat = {}
+for vertex, info in edges_to_join.items():
+    feat_to_del.append(info['line'])
+    old_feat = edge_feat_dict[info['line']]
+    # TODO: fix
+    if info['line'] in new_feat.keys():
+        old_feat = new_feat[info['line']].geometryAndOwnership()
+    geom = old_feat.geometry()
+    geom.moveVertex(info['closest_point'][0], info['closest_point'][1], info['index'])
+    feat = QgsFeature()
+    feat.setGeometry(geom)
+    feat.setAttributes(old_feat.attributes())
+    new_feat[info['line']] = feat
+
+
+new_feat_list = [j for i,j in new_feat.items()]
+os_road_sel.startEditing()
+os_road_sel.dataProvider().deleteFeatures(feat_to_del)
+os_road_sel.addFeatures(new_feat_list)
+os_road_sel.commitChanges()
+
+# TODO: insert vertex on meridian layer where i have the new points
+break_points = {}
+meridian_feat_dict = {i.id(): i for i in meridian.getFeatures()}
+for k, v in edges_to_join.items():
+    geom_pl = meridian_geom_dict[v['closest_neigh'][0]].asPolyline()
+    for i,j in enumerate(geom_pl):
+        if i != 0:
+            vertexBef_x = geom_pl[(i - 1) % len(geom_pl)][0]
+            vertexAfter_x = geom_pl[i][0]
+            # TODO: what if they are negative
+            if k[0] <= vertexBef_x and k[0] >= vertexAfter_x:
+                indBefVertex = [(i - 1) % len(geom_pl)]
+                break
+    break_points[v['closest_neigh'][0]] = {'new_vertex':v['closest_point'], 'indBefVertex': indBefVertex }
+
+feat_to_del = [k for k, v in break_points.items()]
+new_feat = {}
+for k, v in break_points.items():
+    # TODO: fix if geom has more than one breakage points
+    feat = QgsFeature()
+    geom = meridian_feat_dict[k].geometryAndOwnership()
+    if not geom.insertVertex(v['new_vertex'][0], v['new_vertex'][1], v['indBefVertex'][0]):
+        print k
+    feat.setGeometry(geom)
+    feat.setAttributes(meridian_feat_dict[k].attributes())
+    new_feat[k] = feat
+
+new_feat_list = [j for i, j in new_feat.items()]
+meridian.startEditing()
+meridian.dataProvider().deleteFeatures(feat_to_del)
+meridian.addFeatures(new_feat_list)
+meridian.commitChanges()
+
+
+
+
+# TODO: check deadends but only the ones close to the meridian
+edges_to_check = []
+for e in os_road_graph.edges(data=True):
+    if e[0] in roadEnds.keys() or e[1] in roadEnds.keys():
+        edges_to_check.append(e[2]['feat_id'])
+
+os_road_sel = iface.mapCanvas().currentLayer()
+os_road_sel.select(edges_to_join)
+
+
+
+
+
+
+# get index/indices of line to move on meridian network
+
+
+
+
+
+
+# TODO: use v.clean to snap endpoints from the meridian and endpoints of the os road layer after joining them together
+# processing.runandload("grass:v.clean", input, 2, 1, None, None, None, output, error_output)
 
 
