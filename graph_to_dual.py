cbdb085cd018feb7474556b9b0dfac2d54f97b8a
Ioanna <ioanna.kolovou@gmail.com>
2016-09-26 08:44:26 +0100

    added iterators

--- graph_to_dual.py
+++ graph_to_dual.py
@@ -1,27 +1,46 @@
 import itertools
 import networkx as nx
 
-def graph_to_dual(snapped_graph, id_column, inter_to_inter=False):
-    # construct a dual graph with all connections
-    dual_graph_edges = []
-    # all lines
-    if not inter_to_inter:
-        for i, j in snapped_graph.adjacency_iter():
-            edges = []
-            for k, v in j.items():
-                edges.append(v[0][id_column])
-            dual_graph_edges += [x for x in itertools.combinations(edges, 2)]
-    # only lines with connectivity 2
-    if inter_to_inter:
-        for i, j in snapped_graph.adjacency_iter():
-            edges = []
-            if len(j) == 2:
-                for k, v in j.items():
-                    edges.append(v[0][id_column])
-            dual_graph_edges += [x for x in itertools.combinations(edges, 2)]
+
+# make topology iterator ( point_coord : [lines] )
+
+
+def topology_iter(prGraph, id_column, break_at_intersections):
+    if break_at_intersections:
+        for i, j in prGraph.adjacency_iter():
+            edges = [v[0][id_column] for k, v in j.items() if len(j) == 2]
+            yield i, edges
+    else:
+        for i, j in prGraph.adjacency_iter():
+            edges = [v[0][id_column] for k, v in j.items()]
+            yield i, edges
+
+
+# make iterator of dual graph edges from prGraph edges
+
+
+def dl_edges_from_pr_graph(prGraph, id_column, break_at_intersections):
+    for point,edges in topology_iter(prGraph,id_column,break_at_intersections):
+        for x in itertools.combinations(edges, 2):
+            yield x
+
+
+
+# make iterator of dual graph nodes from prGraph edges
+
+
+def dl_nodes_from_pr_graph(prGraph, dlGrpah, id_column):
+    for e in prGraph.edges_iter(data=id_column):
+        if e[2] not in dlGrpah.nodes():
+            yield e[2]
+
+
+# convert primal graph to dual graph
+
+
+def graph_to_dual(primal_graph, id_column, break_at_intersections=False):
     dual_graph = nx.MultiGraph()
-    dual_graph.add_edges_from(dual_graph_edges)
+    dual_graph.add_edges_from([edge for point,edge in dl_edges_from_pr_graph(primal_graph, id_column, break_at_intersections)])
     # add nodes (some lines are not connected to others because they are pl)
-    for e in snapped_graph.edges_iter(data=id_column):
-        dual_graph.add_node(e[2])
+    dual_graph.add_nodes_from([node for node in dl_nodes_from_pr_graph(primal_graph, dual_graph, id_column)])
     return dual_graph
