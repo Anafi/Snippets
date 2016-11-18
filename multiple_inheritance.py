9719391d9820ceeaae320341d3a80e8d31f31041
joe <ioanna.kolovou@gmail.com>
2016-11-18 14:56:57 +0000

    added source

--- multiple_inheritance.py
+++ multiple_inheritance.py
@@ -1,4 +1,24 @@
 
+# source: http://stackoverflow.com/questions/3277367/how-does-pythons-super-work-with-multiple-inheritance
+class First(object):
+    def __init__(self):
+        print "first"
+
+class Second(First):
+    def __init__(self):
+        print "second"
+
+class Third(First):
+    def __init__(self):
+        print "third"
+
+class Fourth(Second, Third):
+    def __init__(self):
+        super(Fourth, self).__init__()
+        print "that's it"
+
+
+
 from PyQt4.QtCore import QObject
 #class First(object):
 #    def __init__(self):
